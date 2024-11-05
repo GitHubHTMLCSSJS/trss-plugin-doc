36ab87520b8a42f179a0b347547f9f0efa024e48
qintools <fqy20100531@163.com>
2024-11-05 19:40:29 +0800

    Delete src/start.sh

--- src/start.sh
+++
@@ -1,4 +0,0 @@
-mkdir practice              
-cd practice
-pnpm add express                 
-curl -O https://mirrors.chenby.cn/https://raw.githubusercontent.com/GitHubHTMLCSSJS/trss-plugin-doc/main/src/app.js node app.js
